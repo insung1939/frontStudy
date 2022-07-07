2f27112984f73a0b29bfe8781f00852329d1bc4a
insung2915 <88409904+insung2915@users.noreply.github.com>
2022-07-07 09:42:04 +0900

    react todo app js

---
+++ todo-app/src/components/TodoListItem.js
@@ -0,0 +1,20 @@
+import { MdCheckBoxOutlineBlank, MdCheckBox, MdRemoveCircleOutline } from "react-icons/md";
+import './TodoListItem.scss'
+import cn from "classnames";
+
+const TodoListItem = ({ todo, onRemove, onToggle }) => {
+    const { id, text, checked } = todo
+    return (
+        <div className="TodoListItem">
+            <div className={cn('checkbox', { checked })} onToggle={() => { onToggle(id) }}>
+                {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
+                <div className="text">{text}</div>
+            </div>
+            <div className="remove" onClick={() => onRemove(id)}>
+                <MdRemoveCircleOutline />
+            </div>
+        </div>
+    )
+}
+
+export default TodoListItem
